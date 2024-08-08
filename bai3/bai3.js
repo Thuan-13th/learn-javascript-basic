227a98dc4f1102a6d67cb86c71562a70decc73f6
Ngô Đức Thuận <156276083+Thuan-13th@users.noreply.github.com>
2024-08-08 17:46:29 +0700

    bai4

--- bai3/bai3.js
+++
@@ -1,7 +0,0 @@
-
-let c = 5;
-let d = 9.5;
-console.log(` Hello bai 3 "' : `,c==d);
-
-let x=5,y = 6, z = 7;
-console.log("Print: ","x = ", x ,"y = ", y ,"z = ", z);
---
+++ bai4/bai4.js
@@ -0,0 +1,4 @@
+let a = "Tôi là String - Chuỗi";
+console.log(a);
+console.log(4+5+'a');   // 9a
+console.log('a'+4+5);   //a45
